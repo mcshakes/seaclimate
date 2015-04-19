5eaca2cd4471aa12702603b130e1001061764726
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-19 13:24:25 -0600

    routed static pages home. nothing really to test. Instead of assert_selecting in the controller, I may do some Capy tests to check everything out.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root to: "static_pages#home"
 
   get 'static_pages/about'
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    # assert_select "title", "Giant Unknown Title"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    # assert_select "title", "About Us"
   end
 
 end
