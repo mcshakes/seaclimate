2fb188447e34fa7eb0a4f4fecfb16fb20e045050
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-22 09:41:18 -0600

    migrated controller for dashboard and routed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "static_pages#home"
-
   get 'static_pages/about'
+  get "/dashboard", to: "dashboard#show"
 
-
+  # get "/auth/:provider/callback", to: "sessions#create"
 end
