8e67d3574c99c3e7774b938bf741232f7c5df059
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-22 11:21:49 -0600

    so the dashboard button takes them to the index, where they could login with oauth accounts and take them to random dashboard#show pages. This seems like a legit response

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twiiter, "KEY", "SECRET"
+  provider :twitter, "09l6AkYABNnDy643xbL7yBzlM", "EIVD6H0fnwknvIGb9zCspwb8Tn5EPoByfRXnRIYJf0KIVM9185"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "static_pages#home"
   get 'static_pages/about'
-  get "/dashboard", to: "dashboard#show"
+  # get "/dashboard", to: "dashboard#show"
 
+  resources "dashboard"
   get "/auth/:provider/callback", to: "sessions#create"
 end
