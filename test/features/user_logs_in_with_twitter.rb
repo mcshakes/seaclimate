47b7454e0c82f4000ee8001a2db4f326899d1c44
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-24 12:30:24 -0600

    added a bunch more for setup. Confused about placement

--- test/features/user_logs_in_with_twitter.rb
+++ test/features/user_logs_in_with_twitter.rb
@@ -3,5 +3,8 @@ require 'test_helper'
 class UserLoginTwitterTest < ActionDispatch::IntegrationTest
   include Capybara::DSL
 
-  
+  def setup
+    
+    OmniAuth.config.mock_auth[:twitter] = nil
+  end
 end
