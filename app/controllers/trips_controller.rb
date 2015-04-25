0cd8ae4f5f173faf6a6f7ccb00b5fec90aa94983
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-25 13:20:22 -0600

    working on create trip form. I'll style later. For now, let's lest the trip on the index page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,9 +4,27 @@ class TripsController < ApplicationController
 
   end
 
-  def create
-    @trip = Trip.new(params[:])
+  def new
+    @trip = Trip.new
   end
 
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      flash[:notice] = "Trip Created!"
+      redirect_to trip_path
+    else
+      flash[:notice] = "Shit happened..."
+      redirect_to trip_path
+    end
+  end
+
+
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :long, :lat)
+  end
 
 end
