50f81a6f5bd5fd8d57a54763ba8ca400a87beb1a
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-27 21:54:54 -0600

    fixed somewhat the fact that the app isn't finding the climate data all the time. Even if it finds it within the Rails Console. Negatives are weird sometimes

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,13 +1,19 @@
 class Weather < ActiveRecord::Base
   belongs_to :trip
 
+
   def create_from_weather_service
-    self.description = weather["weather"].first["description"]
-    save!
+    if !weather["weather"].nil?
+      self.description  = weather["weather"].first["description"]
+      self.temp         = weather["main"]["temp"]
+      self.wind_speed   = weather["wind"]["speed"]
+      save!
+    end
   end
 
   def weather_summary
-    "The weather is #{description}"
+    "The weather is #{description}, with a temperature of #{temp} and
+    wind speeds of #{wind_speed}"
   end
 
   private
