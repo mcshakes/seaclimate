42045229244c013aeedbd143742a5e3afc41e7a6
Edward McCarthy <efmccarthy26@gmail.com>
2015-04-30 13:25:35 -0600

    removing more premadonna methods

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -8,7 +8,8 @@ class Weather < ActiveRecord::Base
       self.temp         = weather["main"]["temp"]
       self.wind_speed   = weather["wind"]["speed"]
       self.degree     = weather["wind"]["deg"]
-      self.cloud      = weather["clouds"]["all"]
+      # self.max_temp   = weather["main"]["temp_max"]
+      # self.cloud      = weather["clouds"]["all"]
       save!
     end
   end
